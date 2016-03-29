7ff1ed7f8c8c3b71007ffdaf7316154b777e76ec
Sergey Vorobyev <sevoro.sv@gmail.com>
2016-03-29 15:16:03 +0500

    Added NewsTab classes for DI and MVP

--- app/src/main/java/com/naks/vk/view/fragment/BaseFragment.java
+++ app/src/main/java/com/naks/vk/view/fragment/BaseFragment.java
@@ -4,16 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
-import com.naks.vk.App;
-import com.naks.vk.di.component.AppComponent;
+import com.naks.vk.di.component.MainComponent;
+import com.naks.vk.view.activity.MainActivity;
 
 public abstract class BaseFragment extends Fragment {
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        setupComponent(App.get(getActivity()).getComponent());
+        setupComponent(((MainActivity)getActivity()).getComponent());
+        //setupComponent(App.get(getActivity()).getComponent());
     }
 
-    protected abstract void setupComponent(AppComponent appComponent);
+    protected abstract void setupComponent(MainComponent component);
 }
