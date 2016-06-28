3e477db80dcfb043cf996869ce5ca18edf9ff5a8
Sergey Vorobyev <sevoro.sv@gmail.com>
2016-06-28 14:44:44 +0600

    Added processing #hashtags for text in NewsItem; Refactoring;

--- app/src/main/java/com/naks/vk/ui/adapter/NewsRecyclerAdapter.java
+++ app/src/main/java/com/naks/vk/ui/adapter/NewsRecyclerAdapter.java
@@ -4,8 +4,15 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +22,7 @@ import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.naks.vk.R;
@@ -23,6 +31,12 @@ import com.naks.vk.api.domain.VKApiNews;
 import com.vk.sdk.api.model.VKApiCommunityFull;
 import com.vk.sdk.api.model.VKApiUserFull;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -30,9 +44,10 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     private static final int VISIBLE_THRESHOLD = 10;
 
-    private static final int VIEW_ITEM = 0;
-    private static final int VIEW_PROGRESS = 1;
-    private static final int VIEW_ERROR_LOAD_PAGE = 3;
+    private static final int VIEW_TYPE_ITEM = 0;
+    private static final int VIEW_TYPE_PROGRESS = 1;
+    private static final int VIEW_TYPE_ERROR_LOAD_PAGE = 3;
+
     private static final int MAX_COUNT_WORDS = 40;
 
     private static final int ANIMATE_EXPAND_DURATION = 200;
@@ -54,13 +69,13 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         switch (viewType) {
-            case VIEW_ITEM:
+            case VIEW_TYPE_ITEM:
                 return new ItemViewHolder(LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.news_item, parent, false));
-            case VIEW_PROGRESS:
+            case VIEW_TYPE_PROGRESS:
                 return new ProgressViewHolder(LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.progress_item, parent, false));
-            case VIEW_ERROR_LOAD_PAGE:
+            case VIEW_TYPE_ERROR_LOAD_PAGE:
                 return new ErrorViewHolder(LayoutInflater.from(parent.getContext())
                         .inflate(R.layout.error_item, parent, false));
         }
@@ -80,67 +95,47 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             return;
         }
 
-        if (data != null && data.items != null) {
+        ItemViewHolder vh = (ItemViewHolder) holder;
+        vh.item = data.items.get(position);
 
-            ItemViewHolder vh = (ItemViewHolder) holder;
-            vh.item = data.items.get(position);
-
-            int sourceId = vh.item.source_id;
-            if (sourceId > 0) {
-                vh.user = findUsersByItem(sourceId);
-            } else {
-                vh.group = findGroupByItem(sourceId);
-            }
-            assert vh.user != null;
-            assert vh.group != null;
-
-            Glide.with(context)
-                    .load(sourceId > 0 ? vh.user.photo_100 : vh.group.photo_100)
-                    .centerCrop()
-                    .crossFade()
-                    .into(vh.headImage);
-
-            vh.owner.setText(sourceId > 0 ?
-                    vh.user.first_name + " " + vh.user.last_name :
-                    vh.group.name);
-
-            vh.date.setText(String.valueOf(vh.item.date));
-
-            if (!vh.item.text.isEmpty()) {
-
-                vh.expandButton.setOnClickListener(view -> expandWithAnimation(
-                        vh.collapsedText, vh.expandedText, vh.expandButton, vh.item.text));
-
-                String[] words = vh.item.text.split(" ");
-                if (words.length > MAX_COUNT_WORDS) {
-                    vh.collapsedText.setVisibility(View.VISIBLE);
-                    vh.expandedText.setVisibility(View.GONE);
-                    vh.expandButton.setVisibility(View.VISIBLE);
-
-                    final ViewGroup.LayoutParams layoutParams = vh.expandButton.getLayoutParams();
-                    layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-                    vh.expandButton.setLayoutParams(layoutParams);
-
-                    StringBuilder sb = new StringBuilder();
-                    for (int i=0; i<MAX_COUNT_WORDS; i++) {
-                        sb.append(words[i]);
-                        sb.append(i != MAX_COUNT_WORDS - 1 ? " " : "…");
-                    }
-                    vh.collapsedText.setText(sb.toString());
-                } else {
-                    vh.collapsedText.setVisibility(View.GONE);
-                    vh.expandButton.setVisibility(View.GONE);
-                    vh.expandedText.setVisibility(View.VISIBLE);
-                    vh.expandedText.setText(vh.item.text);
-                }
-            } else {
-                vh.collapsedText.setVisibility(View.GONE);
-                vh.expandedText.setVisibility(View.GONE);
-                vh.expandButton.setVisibility(View.GONE);
-            }
-
-            vh.v.setOnClickListener(v -> newsItemClickListener.onClick(vh.item, vh.user, vh.group));
+        int sourceId = vh.item.source_id;
+        if (sourceId > 0) {
+            vh.user = findUsersByItem(sourceId);
+        } else {
+            vh.group = findGroupByItem(sourceId);
         }
+        assert vh.user != null;
+        assert vh.group != null;
+
+        Glide.with(context)
+                .load(sourceId > 0 ? vh.user.photo_100 : vh.group.photo_100)
+                .centerCrop()
+                .crossFade()
+                .into(vh.headImage);
+
+        vh.owner.setText(sourceId > 0 ? vh.user.first_name + " " + vh.user.last_name :
+                vh.group.name);
+
+        String date = String.valueOf(vh.item.date);
+        // TODO: 28.06.2016: Implement pars date
+        vh.date.setText(date);
+
+        vh.ownerLayout.setOnClickListener(view1 -> {
+            Toast.makeText(context, "clickOnHeader", Toast.LENGTH_SHORT).show();
+            // TODO: 28.06.2016: Implement showing owner (profile or group)
+        });
+
+        if (!vh.item.text.isEmpty()) {
+            processAndPresentText(vh.collapsedText, vh.expandedText, vh.expandButton, vh.item.text);
+            vh.expandButton.setOnClickListener(view -> expandWithAnimation(vh.collapsedText,
+                    vh.expandedText, vh.expandButton, vh.item.text));
+        } else {
+            vh.collapsedText.setVisibility(View.GONE);
+            vh.expandedText.setVisibility(View.GONE);
+            vh.expandButton.setVisibility(View.GONE);
+        }
+
+        vh.v.setOnClickListener(v -> newsItemClickListener.onClick(vh.item, vh.user, vh.group));
     }
 
     @Override
@@ -225,9 +220,9 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     public int getItemViewType(int position) {
         VKApiItem item = data.items.get(position);
         if (item != null) {
-            return VIEW_ITEM;
+            return VIEW_TYPE_ITEM;
         } else {
-            return isShowError ? VIEW_ERROR_LOAD_PAGE : VIEW_PROGRESS;
+            return isShowError ? VIEW_TYPE_ERROR_LOAD_PAGE : VIEW_TYPE_PROGRESS;
         }
     }
 
@@ -250,6 +245,7 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
     public class ItemViewHolder extends RecyclerView.ViewHolder {
         public final View v;
 
+        @BindView(R.id.ownerLayout) View ownerLayout;
         @BindView(R.id.headImage) ImageView headImage;
         @BindView(R.id.owner) TextView owner;
         @BindView(R.id.date) TextView date;
@@ -286,8 +282,72 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
         }
     }
 
-    public static void expandWithAnimation(TextView collapsedView, TextView expandedView,
-                                            TextView button, String text) {
+    private void processAndPresentText(View collapsedText, View expandedText, View expandButton, String text) {
+
+        String[] words = text.split(" ");
+
+        if (words.length > MAX_COUNT_WORDS) {
+            collapsedText.setVisibility(View.VISIBLE);
+            expandedText.setVisibility(View.GONE);
+            expandButton.setVisibility(View.VISIBLE);
+
+            final ViewGroup.LayoutParams layoutParams = expandButton.getLayoutParams();
+            layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            expandButton.setLayoutParams(layoutParams);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i=0; i<MAX_COUNT_WORDS; i++) {
+                sb.append(words[i]);
+                sb.append(i != MAX_COUNT_WORDS - 1 ? " " : "…");
+            }
+            TextView collapsedTV = (TextView) collapsedText;
+            collapsedTV.setText(textToSpannable(sb.toString()));
+            collapsedTV.setMovementMethod(LinkMovementMethod.getInstance());
+        } else {
+            collapsedText.setVisibility(View.GONE);
+            expandButton.setVisibility(View.GONE);
+            expandedText.setVisibility(View.VISIBLE);
+            TextView expandedTV = (TextView) expandedText;
+            expandedTV.setText(textToSpannable(text));
+            expandedTV.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+    }
+
+    private Spannable textToSpannable(String text) {
+        Spannable result = new SpannableString(text);
+        int color = ContextCompat.getColor(context, R.color.colorAccent);
+
+        Pattern hashTagsPattern = Pattern.compile("#(\\S+)");
+        Matcher mather = hashTagsPattern.matcher(text);
+        List<String> hashTags = new ArrayList<>();
+        while (mather.find()) {
+            String tag = mather.group(1);
+            String[] tags = tag.split("#");
+            if (tags.length > 1) {
+                hashTags.addAll(Arrays.asList(tags));
+            } else hashTags.add(tag);
+        }
+
+        for (String hashTag : hashTags) {
+            int startPos, endPos; //span positions
+            startPos = text.indexOf(hashTag) - 1;
+            endPos = startPos + hashTag.length() + 1;
+            ForegroundColorSpan colorSpan = new ForegroundColorSpan(color);
+            Object clickableSpan = new ClickableSpan() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(context, hashTag, Toast.LENGTH_SHORT).show();
+                    //TODO 28.06.2016: Implement searching by tag
+                }
+            };
+            result.setSpan(colorSpan, startPos, endPos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            result.setSpan(clickableSpan, startPos, endPos, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+
+        return result;
+    }
+
+    private void expandWithAnimation(View collapsedView,View expandedView, View button, String text) {
 
         int[] startHeight = new int[1];
 
@@ -326,7 +386,9 @@ public class NewsRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
             }
         });
 
-        expandedView.setText(text);
+        TextView expandedTV = (TextView) expandedView;
+        expandedTV.setText(textToSpannable(text));
+        expandedTV.setMovementMethod(LinkMovementMethod.getInstance());
         expandedView.setVisibility(View.VISIBLE);
         collapsedView.setVisibility(View.GONE);
 
