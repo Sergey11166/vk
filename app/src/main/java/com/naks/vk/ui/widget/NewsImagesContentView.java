fd4d06883ede5eae1c17c4afe70d7a9d9ac49269
Sergey Vorobyev <sevoro.sv@gmail.com>
2016-07-14 00:40:04 +0500

    Fix bug in NewsImagesContentView: present 1 image wrong;

--- app/src/main/java/com/naks/vk/ui/widget/NewsImagesContentView.java
+++ app/src/main/java/com/naks/vk/ui/widget/NewsImagesContentView.java
@@ -2,21 +2,15 @@ package com.naks.vk.ui.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 import android.view.ViewManager;
-import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
-import com.naks.vk.R;
 import com.vk.sdk.api.model.VKApiPhoto;
 
 import java.util.ArrayList;
@@ -24,7 +18,8 @@ import java.util.List;
 
 public class NewsImagesContentView extends RelativeLayout {
 
-    List<ImageView> views;
+    private List<ImageView> views;
+    private float scale;
 
     public NewsImagesContentView(Context context) {
         super(context);
@@ -42,6 +37,7 @@ public class NewsImagesContentView extends RelativeLayout {
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @SuppressWarnings("unused")
     public NewsImagesContentView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         init();
@@ -49,6 +45,7 @@ public class NewsImagesContentView extends RelativeLayout {
 
     private void init() {
         views = new ArrayList<>();
+        scale = getContext().getResources().getDisplayMetrics().density;
     }
 
     public void setImages(List<VKApiPhoto> images) {
@@ -63,19 +60,19 @@ public class NewsImagesContentView extends RelativeLayout {
         views.clear();
         switch (images.size()) {
             case 1:
+                VKApiPhoto photo = images.get(0);
                 ImageView imageView = new ImageView(getContext());
-                LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                int width = (int) (350 * scale);
+                int height = width * photo.height / photo.width;
+                LayoutParams params = new LayoutParams(width, height);
                 addView(imageView, params);
                 views.add(imageView);
-                VKApiPhoto photo = images.get(0);
-                Bitmap bitmap = Bitmap.createBitmap(photo.width, photo.height, Bitmap.Config.RGB_565);
-                bitmap.eraseColor(Color.LTGRAY);
                 Glide.with(getContext())
                         .load(photo.photo_604)
+                        .placeholder(new ColorDrawable(Color.LTGRAY))
                         .into(imageView);
-                imageView.setImageBitmap(bitmap);
-                imageView.setBackgroundColor(Color.GRAY);
                 return;
+
             case 2:
 
                 return;
