c55ccd6a8462bc4bc634cd4be13220a8e99e80bf
Sergey Vorobyev <sevoro.sv@gmail.com>
2016-07-15 02:25:58 +0500

    In NewsImagesContentView:     reworked handle 2 images;     added handling 3 images;     refactoring;

--- app/src/main/java/com/naks/vk/ui/widget/NewsImagesContentView.java
+++ app/src/main/java/com/naks/vk/ui/widget/NewsImagesContentView.java
@@ -15,6 +15,7 @@ import com.vk.sdk.api.model.VKApiPhoto;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class NewsImagesContentView extends RelativeLayout {
@@ -61,13 +62,13 @@ public class NewsImagesContentView extends RelativeLayout {
         views.clear();
         switch (images.size()) {
             case 1:
-                handle1(images);
+                allImagesToLine(images);
                 return;
             case 2:
-                handle2(images);
+                allImagesToLine(images);
                 return;
             case 3:
-                handle3(images);
+                handle3Images(images);
                 return;
             case 4:
 
@@ -92,57 +93,34 @@ public class NewsImagesContentView extends RelativeLayout {
         }
     }
 
-    private void handle1(List<VKApiPhoto> images) {
-        VKApiPhoto photo = images.get(0);
-        ImageView imageView = new ImageView(getContext());
-        int width = (int) (IMAGES_WIDTH * scale);
-        int height = width * photo.height / photo.width;
-        LayoutParams params = new LayoutParams(width, height);
-        addView(imageView, params);
-        views.add(imageView);
-        Glide.with(getContext())
-                .load(photo.photo_604)
-                .placeholder(new ColorDrawable(Color.LTGRAY))
-                .into(imageView);
-    }
+    private void fillImagesToViewsInLine(List<VKApiPhoto> images, ImageView[] views, LayoutParams[] params) {
+        final int[] widths = new int[images.size()];
+        final int[] heights = new int[images.size()];
 
-    private void handle2(List<VKApiPhoto> images) {
-        final int count2 = images.size();
-        final LayoutParams[] params2 = new LayoutParams[count2];
-        final int[] widths2 = new int[count2];
-        final int[] heights2 = new int[count2];
-        final ImageView[] views = new ImageView[count2];
-        for(int i = 0; i< count2; i++) {
-            views[i] = new ImageView(getContext());
-            views[i].setScaleType(ImageView.ScaleType.CENTER_CROP);
-            views[i].setId(View.generateViewId());
+        int[] widthNorm = new int[images.size()];
+        int widthSum = widthNorm[0] = images.get(0).width;
+        for (int i=1; i<images.size(); i++) {
+            widthNorm[i] = images.get(0).height * images.get(i).width / images.get(i).height;
+            widthSum += widthNorm[i];
         }
 
-        boolean isHorizontal = false;
-        for (int i=0; i<count2; i++)
-            isHorizontal = images.get(i).height >= images.get(i).width;
-
-        int heightAverageSum = 0;
-        if (isHorizontal) {
-            for(int i=0; i<count2; i++) {
-                widths2[i] = (int) ((IMAGES_WIDTH / 2 - 1)*scale);
-                heights2[i] = widths2[i] * images.get(i).height / images.get(i).width;
-                heightAverageSum += heights2[i];
-            }
-            for (int i=0; i<count2; i++) heights2[i] = heightAverageSum / count2;
-        } else for(int i = 0; i< count2; i++) {
-            widths2[i] = (int) (IMAGES_WIDTH * scale);
-            heights2[i] = widths2[i] * images.get(i).height / images.get(i).width;
+        for(int i = 0; i<images.size(); i++) {
+            widths[i] = (int) ((IMAGES_WIDTH * widthNorm[i] / widthSum) * scale);
+            heights[i] = widths[0] * images.get(0).height / images.get(0).width;
         }
 
-        for (int i = 0; i< count2; i++) {
-            params2[i] = new LayoutParams(widths2[i], heights2[i]);
+        for (int i = 0; i<images.size(); i++) {
+            params[i].width = widths[i];
+            params[i].height = heights[i];
             if (i != 0) {
-                params2[i].addRule(isHorizontal ? RIGHT_OF : BELOW, views[i - 1].getId());
-                if (isHorizontal) params2[i].setMargins((int) (2 * scale), 0, 0, 0);
-                else params2[i].setMargins(0, (int) (2 * scale), 0, 0);
+                params[i].addRule(RIGHT_OF, views[i - 1].getId());
+                params[i].setMargins(
+                        (int) (2 * scale),
+                        params[i].topMargin,
+                        params[i].rightMargin,
+                        params[i].bottomMargin);
             }
-            addView(views[i], params2[i]);
+            addView(views[i], params[i]);
             this.views.addAll(Arrays.asList(views));
             Glide.with(getContext())
                     .load(images.get(i).photo_604)
@@ -151,7 +129,49 @@ public class NewsImagesContentView extends RelativeLayout {
         }
     }
 
-    private void handle3(List<VKApiPhoto> images) {
+    private void allImagesToLine(List<VKApiPhoto> images) {
+        final LayoutParams[] params = new LayoutParams[images.size()];
+        for (int i=0; i<images.size(); i++) params[i] = new LayoutParams(0, 0);
+        fillImagesToViewsInLine(images, createViewsForImages(images.size()), params);
+    }
 
+    private ImageView[] createViewsForImages(int size) {
+        final ImageView[] views = new ImageView[size];
+        for(int i = 0; i<size; i++) {
+            views[i] = new ImageView(getContext());
+            views[i].setScaleType(ImageView.ScaleType.CENTER_CROP);
+            views[i].setId(View.generateViewId());
+            views[i].setBackgroundColor(Color.YELLOW);
+        }
+        return views;
+    }
+
+    private void handle3Images(List<VKApiPhoto> images) {
+        boolean isAllVertical = true;
+        for (VKApiPhoto image : images) {
+            if (image.width > image.height) {
+                isAllVertical = false;
+                break;
+            }
+        }
+        if (isAllVertical) {
+            allImagesToLine(images);
+            return;
+        }
+        ImageView[] views1 = createViewsForImages(1);
+        LayoutParams[] params1 = new LayoutParams[1];
+        params1[0] = new LayoutParams(0, 0);
+        fillImagesToViewsInLine(Collections.singletonList(images.get(0)), views1, params1);
+
+        ImageView[] views2 = createViewsForImages(images.size() - 1);
+        LayoutParams[] params2 = new LayoutParams[images.size() - 1];
+        for (int i=0; i<params2.length; i++) {
+            params2[i] = new LayoutParams(0,0);
+            params2[i].setMargins(0, (int) (2 * scale), 0, 0);
+            params2[i].addRule(BELOW, views1[0].getId());
+        }
+        List<VKApiPhoto> images2 = new ArrayList<>(images.size() - 1);
+        for (int i=1; i<images.size(); i++) images2.add(images.get(i));
+        fillImagesToViewsInLine(images2, views2, params2);
     }
 }
