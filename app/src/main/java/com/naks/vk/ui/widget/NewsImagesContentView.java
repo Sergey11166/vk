736d4db59f5cda3638c9a2982ccea0839d533269
Sergey Vorobyev <sevoro.sv@gmail.com>
2016-07-14 15:15:51 +0600

    Refactoring

--- app/src/main/java/com/naks/vk/ui/widget/NewsImagesContentView.java
+++ app/src/main/java/com/naks/vk/ui/widget/NewsImagesContentView.java
@@ -7,7 +7,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewManager;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -19,6 +18,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class NewsImagesContentView extends RelativeLayout {
+
+    private static final int IMAGES_WIDTH = 350;
 
     private List<ImageView> views;
     private float scale;
@@ -60,62 +61,13 @@ public class NewsImagesContentView extends RelativeLayout {
         views.clear();
         switch (images.size()) {
             case 1:
-                VKApiPhoto photo = images.get(0);
-                ImageView imageView = new ImageView(getContext());
-                int width = (int) (350 * scale);
-                int height = width * photo.height / photo.width;
-                LayoutParams params = new LayoutParams(width, height);
-                addView(imageView, params);
-                views.add(imageView);
-                Glide.with(getContext())
-                        .load(photo.photo_604)
-                        .placeholder(new ColorDrawable(Color.LTGRAY))
-                        .into(imageView);
+                handle1(images);
                 return;
-
             case 2:
-                final int count2 = images.size();
-                final LayoutParams[] params2 = new LayoutParams[count2];
-                final int[] widths2 = new int[count2];
-                final int[] heights2 = new int[count2];
-                final ImageView[] views = new ImageView[count2];
-                for(int i = 0; i< count2; i++) {
-                    views[i] = new ImageView(getContext());
-                    views[i].setScaleType(ImageView.ScaleType.FIT_CENTER);
-                    views[i].setId(View.generateViewId());
-                }
-
-                boolean isHorizontal = false;
-                for (int i=0; i<count2; i++)
-                    isHorizontal = images.get(i).height >= images.get(i).width;
-
-                if (isHorizontal) {
-                    int wSum = 0; for(int i = 0; i< count2; i++) wSum += images.get(i).width;
-                    for(int i = 0; i< count2; i++)
-                        widths2[i] = (int) (images.get(i).width * 350 * scale/ wSum);
-                } else {
-                    for(int i = 0; i< count2; i++) widths2[i] = (int) (350 * scale);
-                }
-
-                for (int i = 0; i< count2; i++) {
-                    heights2[i] = widths2[i] * images.get(i).height / images.get(i).width;
-                    params2[i] = new LayoutParams(widths2[i], heights2[i]);
-                    if (i != 0) {
-                        params2[i].addRule(isHorizontal ? RIGHT_OF : BELOW, views[i - 1].getId());
-                        if (isHorizontal) params2[i].setMargins((int) (2 * scale), 0, 0, 0);
-                        else params2[i].setMargins(0, (int) (2 * scale), 0, 0);
-                    }
-                    addView(views[i], params2[i]);
-                    this.views.addAll(Arrays.asList(views));
-                    Glide.with(getContext())
-                            .load(images.get(i).photo_604)
-                            .placeholder(new ColorDrawable(Color.LTGRAY))
-                            .into(views[i]);
-                }
+                handle2(images);
                 return;
-
             case 3:
-
+                handle3(images);
                 return;
             case 4:
 
@@ -138,5 +90,68 @@ public class NewsImagesContentView extends RelativeLayout {
             case 10:
 
         }
+    }
+
+    private void handle1(List<VKApiPhoto> images) {
+        VKApiPhoto photo = images.get(0);
+        ImageView imageView = new ImageView(getContext());
+        int width = (int) (IMAGES_WIDTH * scale);
+        int height = width * photo.height / photo.width;
+        LayoutParams params = new LayoutParams(width, height);
+        addView(imageView, params);
+        views.add(imageView);
+        Glide.with(getContext())
+                .load(photo.photo_604)
+                .placeholder(new ColorDrawable(Color.LTGRAY))
+                .into(imageView);
+    }
+
+    private void handle2(List<VKApiPhoto> images) {
+        final int count2 = images.size();
+        final LayoutParams[] params2 = new LayoutParams[count2];
+        final int[] widths2 = new int[count2];
+        final int[] heights2 = new int[count2];
+        final ImageView[] views = new ImageView[count2];
+        for(int i = 0; i< count2; i++) {
+            views[i] = new ImageView(getContext());
+            views[i].setScaleType(ImageView.ScaleType.CENTER_CROP);
+            views[i].setId(View.generateViewId());
+        }
+
+        boolean isHorizontal = false;
+        for (int i=0; i<count2; i++)
+            isHorizontal = images.get(i).height >= images.get(i).width;
+
+        int heightAverageSum = 0;
+        if (isHorizontal) {
+            for(int i=0; i<count2; i++) {
+                widths2[i] = (int) ((IMAGES_WIDTH / 2 - 1)*scale);
+                heights2[i] = widths2[i] * images.get(i).height / images.get(i).width;
+                heightAverageSum += heights2[i];
+            }
+            for (int i=0; i<count2; i++) heights2[i] = heightAverageSum / count2;
+        } else for(int i = 0; i< count2; i++) {
+            widths2[i] = (int) (IMAGES_WIDTH * scale);
+            heights2[i] = widths2[i] * images.get(i).height / images.get(i).width;
+        }
+
+        for (int i = 0; i< count2; i++) {
+            params2[i] = new LayoutParams(widths2[i], heights2[i]);
+            if (i != 0) {
+                params2[i].addRule(isHorizontal ? RIGHT_OF : BELOW, views[i - 1].getId());
+                if (isHorizontal) params2[i].setMargins((int) (2 * scale), 0, 0, 0);
+                else params2[i].setMargins(0, (int) (2 * scale), 0, 0);
+            }
+            addView(views[i], params2[i]);
+            this.views.addAll(Arrays.asList(views));
+            Glide.with(getContext())
+                    .load(images.get(i).photo_604)
+                    .placeholder(new ColorDrawable(Color.LTGRAY))
+                    .into(views[i]);
+        }
+    }
+
+    private void handle3(List<VKApiPhoto> images) {
+
     }
 }
